f0af3c88fb36ac9d249ede4753db29a37afca772
georgelucas <georgelucas@iMac-de-georgelucas.local>
2019-03-17 16:17:57 -0300

    v5

--- src/components/create-client-form/create-client-form.ts
+++ src/components/create-client-form/create-client-form.ts
@@ -1,4 +1,4 @@
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,8 +9,27 @@ export class CreateClientFormComponent {
 
   createClientForm: FormGroup
 
-  constructor() {
+  constructor(
+    public formbuilder: FormBuilder,
+  ) {
+    this.createClientForm = this.formbuilder.group({
+      razaosocial:[null,[Validators.required,Validators.minLength(10)]],
+      fantasia:[null,[Validators.required,Validators.minLength(10)]],
+      cnpj:[null,[Validators.required,Validators.minLength(14),Validators.maxLength(14)]],
+      cep:[null,[Validators.required,Validators.minLength(8),Validators.maxLength(8)]],
+      endereco:[null,[Validators.required,Validators.minLength(10)]],
+      numero:[null,[Validators.required]],
+      bairro:[null,[Validators.required]],
+      cidade:[null,[Validators.required]],
+      estado:[null,[Validators.required]],
+      telefone:[null,[Validators.required]],
+      categoria:[null,[Validators.required]]
+      
+    })
 
+  }
+  cadastrarCliente(){
+    console.log(this.createClientForm.value);
   }
 
 }
